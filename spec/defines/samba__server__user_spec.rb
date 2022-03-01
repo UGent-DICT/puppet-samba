c1f409d8423ceb94e2cf9519d6d8e553e09f72f4
Jan Vansteenkiste <jan@vstone.eu>
2022-03-01 15:28:26 +0100

    tests: convert samba::server::user to on_supported_os

--- spec/defines/samba__server__user_spec.rb
+++ spec/defines/samba__server__user_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe 'samba::server::user', :type => :define do
+  let(:pre_condition) { 'include samba::server' }
+
   let(:title) { 'test_user' }
   let(:params) {{ :password => 'secret' }}
 
-  it { is_expected.to contain_samba__server__user('test_user') }
-  it { is_expected.to contain_exec('add smb account for test_user').with(
-    :command => '/bin/echo -e \'secret\nsecret\n\' | /usr/bin/pdbedit --password-from-stdin -a \'test_user\'',
-    :unless  => '/usr/bin/pdbedit \'test_user\'',
-    :require => 'User[test_user]',
-    :notify  => 'Class[Samba::Server::Service]'
-  ) }
+  on_supported_os.each do |os, osfacts|
+    context "on #{os}" do
+      let(:facts) { osfacts }
+
+      it { is_expected.to contain_samba__server__user('test_user') }
+      it { is_expected.to contain_exec('add smb account for test_user').with(
+        :command => '/bin/echo -e \'secret\nsecret\n\' | /usr/bin/pdbedit --password-from-stdin -a \'test_user\'',
+        :unless  => '/usr/bin/pdbedit \'test_user\'',
+        :require => 'User[test_user]',
+        :notify  => 'Class[Samba::Server::Service]'
+      ) }
+    end
+  end
 end
