5fdad8e83277f536e947a6829845fd3a16682e47
Jan Vansteenkiste <jan@vstone.eu>
2022-03-01 14:48:55 +0100

    tests: Use on_supported_os for samba::server::ads

--- spec/classes/samba__server__ads_spec.rb
+++ spec/classes/samba__server__ads_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe 'samba::server::ads', :type => :class do
-  let( :facts ) { { :osfamily => 'Debian' } }
-  context "Default config" do
-    it { should contain_exec('join-active-directory') }
-  end
 
-  context "No join" do
-    let ( :params ) { { 'perform_join' => false }}
-    it { should_not contain_exec('join-active-directory') }
-  end
+  on_supported_os.each do |os, osfacts|
+    describe "on #{os}" do
+      let(:facts) { osfacts }
 
-  context "Join 'forced'" do
-    let ( :params ) { { 'perform_join' => true }}
-    it { should contain_exec('join-active-directory') }
+      context "Default config" do
+        it { should contain_exec('join-active-directory') }
+      end
+
+      context "No join" do
+        let ( :params ) { { 'perform_join' => false }}
+        it { should_not contain_exec('join-active-directory') }
+      end
+
+      context "Join 'forced'" do
+        let ( :params ) { { 'perform_join' => true }}
+        it { should contain_exec('join-active-directory') }
+      end
+    end
   end
 end
 
