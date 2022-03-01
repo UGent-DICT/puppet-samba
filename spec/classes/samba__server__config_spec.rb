c3087be93d7705e9827565d6342b89be4211746b
Jan Vansteenkiste <jan@vstone.eu>
2022-03-01 15:29:55 +0100

    tests: convert samba::server::config to on_supported_os

--- spec/classes/samba__server__config_spec.rb
+++ spec/classes/samba__server__config_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
 describe 'samba::server::config', :type => :class do
-  context "on a Debian OS" do
-    let( :facts ) { { :osfamily => 'Debian' } }
 
-    it { should contain_file('/etc/samba/smb.conf').with_owner('root') }
+  test_on = {
+    supported_os: [
+      {
+        'operatingsystem'        => 'Debian',
+        'operatingsystemrelease' => ['10'],
+      },
+    ],
+  }
+
+  let(:pre_condition) { 'include samba::server' }
+
+  on_supported_os(test_on).each do |os, osfacts|
+    context "on #{os}" do
+      let( :facts ) { osfacts }
+
+      it { should contain_file('/etc/samba/smb.conf').with_owner('root') }
+    end
   end
 end
-
